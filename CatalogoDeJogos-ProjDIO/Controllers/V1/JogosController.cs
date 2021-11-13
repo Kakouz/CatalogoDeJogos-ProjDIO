59288f9b55adf81c8d5c48a2263e635ce37975ae
Tiago Prudente <tf.prudente@gmail.com>
2021-11-13 20:57:22 -0300

    Adicionado comentarios na JogosController.cs

--- CatalogoDeJogos-ProjDIO/Controllers/V1/JogosController.cs
+++ CatalogoDeJogos-ProjDIO/Controllers/V1/JogosController.cs
@@ -11,37 +11,42 @@ namespace CatalogoDeJogos_ProjDIO.Controllers.V1
     [ApiController]
     public class JogosController : ControllerBase
     {
-
+        // Obter a lista de jogos completo
         [HttpGet]
         public async Task<ActionResult<List<object>>> Obter()
         {
             return Ok();
         }
 
+        // Obter a lista recebendo um id do jogo com parametro
         [HttpGet("(idJogo:guid)")]
         public async Task<ActionResult<object>> Obter(Guid idJogo)
         {
             return Ok();
         }
 
+        // Inserir um jogo dentro da listagem
         [HttpPost]
         public async Task<ActionResult<object>> InserirJogo(object jogo)
         {
             return Ok();
         }
 
+        // Atualizar algum jogo passando o id do jogo
         [HttpPut("(idJogo:guid)")]
         public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
         {
             return Ok();
         }
 
+        // Atualizar o preço do jogo pelo id
         [HttpPatch("(idJogo:guid)/preco/(preco:double)")]
         public async Task<ActionResult> AtualizarPreco(Guid idJogo, double preco)
         {
             return Ok();
         }
 
+        // Apagar um jogo da listagem
         [HttpDelete("(idJogo:guid)")]
         public async Task<ActionResult> ApagarJogo(Guid idJogo)
         {
