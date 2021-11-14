c605a1dbd236b638d4441f58b744dcfab56fe008
Tiago Prudente <tf.prudente@gmail.com>
2021-11-13 21:16:58 -0300

    Criado a Interface IJogoService

---
+++ CatalogoDeJogos-ProjDIO/Services/IJogoService.cs
@@ -0,0 +1,20 @@
+﻿using CatalogoDeJogos_ProjDIO.InputModels;
+using CatalogoDeJogos_ProjDIO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoDeJogos_ProjDIO.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+
+    }
+}
