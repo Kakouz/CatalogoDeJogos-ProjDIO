d04f91095e157d4b69f75ba9846aa4f91a017d04
Tiago Prudente <tf.prudente@gmail.com>
2021-11-13 21:26:44 -0300

    Adicionado a Logica para obter o jogo pela pagina

--- CatalogoDeJogos-ProjDIO/Controllers/V1/JogosController.cs
+++ CatalogoDeJogos-ProjDIO/Controllers/V1/JogosController.cs
@@ -1,9 +1,11 @@
 ﻿using CatalogoDeJogos_ProjDIO.InputModels;
+using CatalogoDeJogos_ProjDIO.Services;
 using CatalogoDeJogos_ProjDIO.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,11 +15,21 @@ namespace CatalogoDeJogos_ProjDIO.Controllers.V1
     [ApiController]
     public class JogosController : ControllerBase
     {
+        private readonly IJogoService _jogoService;
+        public JogosController(IJogoService jogoService)
+        {
+            _jogoService = jogoService;
+        }
         // Obter a lista de jogos completo
         [HttpGet]
-        public async Task<ActionResult<List<JogoViewModel>>> Obter()
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
-            return Ok();
+            var jogos = await _jogoService.Obter(pagina, quantidade);
+
+            if (jogos.Count() == 0)
+                return NoContent();
+
+            return Ok(jogos);
         }
 
         // Obter a lista recebendo um id do jogo com parametro
